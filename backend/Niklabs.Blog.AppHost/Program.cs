fef82ea4b866c274643b88060d65ec4d18e2204b
Nik Lebn <nlebn001@gmail.com>
2026-04-12 13:51:49 +0200

    1

--- backend/Niklabs.Blog.AppHost/Program.cs
+++ backend/Niklabs.Blog.AppHost/Program.cs
@@ -3,24 +3,10 @@ using DotNetEnv;
 Env.Load("../../.env");
 var builder = DistributedApplication.CreateBuilder(args);
 
-
-// read .env vars
-var pgUser = builder.Configuration["POSTGRES_USER"];
-var pgPass = builder.Configuration["POSTGRES_PASSWORD"];
-var pgDb = builder.Configuration["POSTGRES_DB"];
-var pgPort = builder.Configuration["POSTGRES_PORT"] ?? "5432";
-
-var connectionString = $"Host=localhost;Port={pgPort};Database={pgDb};Username={pgUser};Password={pgPass}";
-
-//InDocker compose 
-//services:
-//  api:
-//    environment:
-//      ConnectionStrings__postgres: Host=postgres;Port=5432;Database=mydb;Username=admin;Password=admin
-var postgres = builder.AddConnectionString("postgres", connectionString);
+var pgConStr = builder.Configuration["ConnectionStrings:postgres"];
 
 builder.AddProject<Projects.Niklabs_Blog_Api>("api")
-    .WithReference(postgres);
+    .WithEnvironment("ConnectionStrings__postgres", pgConStr);
 
 builder.Build().Run();
 
